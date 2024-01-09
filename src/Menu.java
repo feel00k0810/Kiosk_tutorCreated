2967ec27f39ea908206f5f997057b1cbbcd0c33f
ChiliChili-97 <feel00k0810@gmail.com>
2024-01-09 23:24:38 +0900

    Kiosk Sample

---
+++ src/Menu.java
@@ -0,0 +1,13 @@
+class Menu {
+    static int idSeq = 1;
+
+    int id;
+    String name;		// 메뉴 이름
+    String description;	// 메뉴 설명
+
+    Menu(String name, String description) {
+        this.id = idSeq++;
+        this.name = name;
+        this.description = description;
+    }
+}
