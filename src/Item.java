05122c2ee5a743d8514a9699be31d0482ea1e240
Jang SeokBin <71073365+ChiliChili-97@users.noreply.github.com>
2024-01-09 23:25:31 +0900

    Kiosk Sample (#1)

---
+++ src/Item.java
@@ -0,0 +1,9 @@
+public class Item extends Menu {
+
+    Double price;	// 상품 가격
+
+    Item(String name, Double price, String description) {
+        super(name, description);
+        this.price = price;
+    }
+}
